bdfbe8ef79f81335cb594e121ca86db54d485c36
RAJENDRAN ESHWARAN <10699484+RajendranEshwaran@users.noreply.github.com>
2025-09-18 16:17:04 -0400

    Initial Commit

---
+++ ClassiMenuBadges/ClassiMenuBadgesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassiMenuBadgesApp.swift
+//  ClassiMenuBadges
+//
+//  Created by RajayGoms on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ClassiMenuBadgesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
